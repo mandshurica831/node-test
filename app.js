4bc1cd6dd3791a4302053b17985d716a38d73ae1
渡辺りえ <watanabe.r@5sg.jp>
2016-08-08 16:55:28 +0900

    会員登録機能／ログイン・ログアウト機能

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var boards = require('./routes/boards');
+var register = require('./routes/register');
+var login = require('./routes/login');
+var logout = require('./routes/logout');
+
+var setUser = require('./setUser');
 
 var app = express();
 
@@ -22,10 +28,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
+app.use('/', setUser, routes);
 app.use('/', routes);
 app.use('/users', users);
-app.use('/boards', boards);
+app.use('/boards', setUser, boards);
+app.use('/register', register);
+app.use('/login', login);
+app.use('/logout', logout);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
