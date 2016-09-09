f02553781dbb0155e6525bd6e0292af6a2a7fd0d
渡辺りえ <watanabe.r@5sg.jp>
2016-09-09 18:52:01 +0900

    DBテスト

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -9,23 +9,42 @@ var dbConfig = {
 };
 */
 
-//Heroku
-var dbConfig = {
-  host     : 'us-cdbr-iron-east-04.cleardb.net',
-  user     : 'b261359fb916b2',
-  password : '61ef1f35',
-  database : 'heroku_6722ee1e07d3f4d'
-};
 
-var connection = mysql.createConnection(dbConfig);
-
-connection.on('error', function(err) { //エラー受け取るコールバック
-  console.log('db error', err);
-  if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-    module.exports = connection;
-  } else {
-    throw err;
-  }
-});
-
-module.exports = connection;
+module.exports.getConnection = function() {
+    // Test connection health before returning it to caller.
+    if ((module.exports.connection) && (module.exports.connection._socket)
+            && (module.exports.connection._socket.readable)
+            && (module.exports.connection._socket.writable)) {
+        return module.exports.connection;
+    }
+    console.log(((module.exports.connection) ?
+            "UNHEALTHY SQL CONNECTION; RE" : "") + "CONNECTING TO SQL.");
+    var connection = mysql.createConnection({
+        host     : 'us-cdbr-iron-east-04.cleardb.net',
+        user     : 'b261359fb916b2',
+        password : '61ef1f35',
+        database : 'heroku_6722ee1e07d3f4d'
+    });
+    connection.connect(function(err) {
+        if (err) {
+            console.log("SQL CONNECT ERROR: " + err);
+        } else {
+            console.log("SQL CONNECT SUCCESSFUL.");
+        }
+    });
+    connection.on("close", function (err) {
+        console.log("SQL CONNECTION CLOSED.");
+    });
+    connection.on("error", function (err) {
+        console.log("SQL CONNECTION ERROR: " + err);
+    });
+    module.exports.connection = connection;
+    return module.exports.connection;
+}
+// Open a connection automatically at app startup.
+module.exports.getConnection();
+// If you've saved this file as database.js, then get and use the
+// connection as in the following example:
+// var database = require(__dirname + "/database");
+// var connection = database.getConnection();
+// connection.query(query, function(err, results) { ....
