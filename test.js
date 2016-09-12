547e7b79432be269ebbf4b8857fdf293f489d5ac
渡辺りえ <watanabe.r@5sg.jp>
2016-09-12 18:26:48 +0900

    試しに

--- test.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var connection = require('mysqlConnection');
-
-connection.query('SHOW TABLES', function(err, rows) {
-  if(err) console.log('ERROR',err);
-  console.log('rows',rows);
-});
