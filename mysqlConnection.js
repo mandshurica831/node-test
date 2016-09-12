25ec8a5844852aad46d5ac5589c89ab937439358
渡辺りえ <watanabe.r@5sg.jp>
2016-09-12 12:10:52 +0900

    もっきゃい

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,42 +1,49 @@
 var mysql = require('mysql');
 
-/*
-var dbConfig = {
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'chat_test_160801'
-};
-*/
-var db_config = {
-  host     : 'us-cdbr-iron-east-04.cleardb.net',
-  user     : 'b261359fb916b2',
-  password : '61ef1f35',
-  database : 'heroku_6722ee1e07d3f4d'
-};
-var connection; //クライアントオブジェクト
+module.exports.getConnection = function() {
 
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config);
+  /* ローカル用DB
+  var dbConfig = {
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'chat_test_160801'
+  };
+  */
+  var connection = mysql.createConnection({
+    host     : 'us-cdbr-iron-east-04.cleardb.net',
+    user     : 'b261359fb916b2',
+    password : '61ef1f35',
+    database : 'heroku_6722ee1e07d3f4d'
+  });
 
   connection.connect(function(err) {
     if(err) {
-      console.log('error when connecting to db:', err);
+      console.log("SQL CONNECT ERROR: " + err);
       setTimeout(handleDisconnect, 2000);  //接続失敗時リトライ
+    } else {
+      console.log("SQL CONNECT SUCCESSFUL.");
     }
   });
 
-  connection.on('error', function(err) { //エラー受け取るコールバック
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.log('== CONECT ==');
-      handleDisconnect();    //再度接続
-    } else {
-      throw err;
-    }
+  //接続切れたとき
+  connection.on("close", function (err) {
+    console.log("SQL CONNECTION CLOSED: " + err);
   });
+
+  //エラーのとき
+  connection.on('error', function(err) {
+    console.log("SQL CONNECTION ERROR: " + err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.log('=> RECONECT...');
+        //再接続
+        module.exports.getConnection();
+      } else {
+        throw err;
+      }
+    });
   module.exports = connection;
 
 }
 
-handleDisconnect();
+module.exports.getConnection();
