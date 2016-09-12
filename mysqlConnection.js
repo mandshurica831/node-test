5964e92bc1367d66c1b444a95c74f4d180f18c36
渡辺りえ <watanabe.r@5sg.jp>
2016-09-12 16:16:30 +0900

    だめだったので戻す

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,81 +1,52 @@
 
+global.get_SQL_Connection = function() {
 
+  var mysql = require('mysql');
 
-var app = require("express")();
-var mysql = require("mysql");
+  /* ローカル用DB
+  var dbConfig = {
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'chat_test_160801'
+  };
+  */
+  var dbData = {
+    host     : 'us-cdbr-iron-east-04.cleardb.net',
+    user     : 'b261359fb916b2',
+    password : '61ef1f35',
+    database : 'heroku_6722ee1e07d3f4d'
+  };
+  var connection = mysql.createConnection(dbData);
 
-var express = require("express");
-
-var dbData = {
-  host     : 'us-cdbr-iron-east-04.cleardb.net',
-  user     : 'b261359fb916b2',
-  password : '61ef1f35',
-  database : 'heroku_6722ee1e07d3f4d'
-};
-
-module.exports = {
-
-    connection: null,
-
-    connect: function() {
-        this.connection = mysql.createConnection(dbData);
-        this.connection.connect();
-    },
-
-    errorHandler: function(err) {
-        if (err) {
-            throw new Error(err);
-        }
-    },
-
-    query: function(query, params, callback) {
-        this.connection.query(query, params, callback);
-    },
-
-    getOne: function(query, params, callback) {
-        var self = this;
-        this.query(query, params, function(err, rows) {
-            self.errorHandler(err);
-            callback(rows.length < 1 ? null : rows[0]);
-        });
-    },
-
-    getAll: function(query, params, callback) {
-        var self = this;
-        this.query(query, params, function(err, rows) {
-            self.errorHandler(err);
-            callback(rows);
-        });
-    },
-
-    insert: function(table, data, callback) {
-        var self = this;
-        var query = "insert into " + table + " set ?";
-        this.query(query, data, function(err, result, fields) {
-            self.errorHandler(err);
-            callback(result.insertId);
-        });
-    },
-
-    update: function(table, data, id, callback) {
-        var fields = "",
-            params = [];
-        for (var k in data) {
-            fields += k + " = ?,";
-            params.push(data[k]);
-        }
-
-        fields = fields.substr(0, fields.length - 1);
-        params.push(id);
-
-        var self = this;
-        var query = "update " + table + " set " + fields + " where id = ?";
-
-        this.query(query, params, function(err, result) {
-            self.errorHandler(err);
-            callback(result);
-        });
+  connection.connect(function(err) {
+    if(err) {
+      console.log("SQL CONNECT ERROR >> " + err);
+      setTimeout(handleDisconnect, 2000);  //接続失敗時リトライ
+    } else {
+      console.log("SQL CONNECT SUCCESSFUL.");
     }
-};
+  });
 
-module.exports.connect();
+  //エラーのとき
+  connection.on('error', function(err) {
+    console.log("SQL CONNECTION ERROR >> " + err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.log('=> RECONECT...');
+        global.get_SQL_Connection();
+      } else {
+        throw err;
+      }
+    });
+
+  var query = 'SELECT B.board_id, B.user_id, B.title, ifnull(U.user_name, \'名無し\') AS user_name, DATE_FORMAT(B.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards B LEFT OUTER JOIN users U ON B.user_id = U.user_id ORDER BY B.created_at DESC';
+  connection.query(query, function(err, rows) {
+    console.log("========= mysqlConnection.js <44> =========");
+    console.log("==> module");
+    console.log(module.exports);
+    console.log("====================================");
+  });
+  return connection;
+}
+
+module.exports = global.get_SQL_Connection();
