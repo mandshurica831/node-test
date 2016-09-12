7d97534da9f9a727b9b1333c442a695e9f6b4d21
渡辺りえ <watanabe.r@5sg.jp>
2016-09-12 17:58:56 +0900

    エラーを出力

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,16 +1,7 @@
+var mysql = require('mysql');
 
 global.get_SQL_Connection = function() {
 
-  var mysql = require('mysql');
-
-  /* ローカル用DB
-  var dbConfig = {
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'chat_test_160801'
-  };
-  */
   var connection = mysql.createConnection({
     host     : 'us-cdbr-iron-east-04.cleardb.net',
     user     : 'b261359fb916b2',
@@ -18,6 +9,7 @@ global.get_SQL_Connection = function() {
     database : 'heroku_6722ee1e07d3f4d'
   });
 
+  //接続時
   connection.connect(function(err) {
     if(err) {
       console.log("SQL CONNECT ERROR >> " + err);
@@ -27,22 +19,17 @@ global.get_SQL_Connection = function() {
     }
   });
 
-  //接続切れたとき
-  connection.on("close", function (err) {
-    console.log("SQL CONNECTION CLOSED >> " + err);
-  });
-
   //エラーのとき
   connection.on('error', function(err) {
     console.log("SQL CONNECTION ERROR >> " + err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-        console.log('=> RECONECT...');
-        //再接続
-        module.exports = global.get_SQL_Connection();
-      } else {
-        throw err;
-      }
-    });
+      console.log('=> RECONECT...');
+      //再接続
+      module.exports = global.get_SQL_Connection();
+    } else {
+      throw err;
+    }
+  });
 
   return connection;
 }
