f09010defa2885137b8cd7e2ffeaa140415fe970
渡辺りえ <watanabe.r@5sg.jp>
2016-09-12 17:01:56 +0900

    tststststs

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -1,5 +1,4 @@
-
-global.get_SQL_Connection = function() {
+module.exports.get_SQL_Connection = function() {
 
   var mysql = require('mysql');
 
@@ -33,20 +32,12 @@ global.get_SQL_Connection = function() {
     console.log("SQL CONNECTION ERROR >> " + err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
         console.log('=> RECONECT...');
-        module.exports = global.get_SQL_Connection();
+        module.exports.get_SQL_Connection();
       } else {
         throw err;
       }
     });
-
-  var query = 'SELECT B.board_id, B.user_id, B.title, ifnull(U.user_name, \'名無し\') AS user_name, DATE_FORMAT(B.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards B LEFT OUTER JOIN users U ON B.user_id = U.user_id ORDER BY B.created_at DESC';
-  connection.query(query, function(err, rows) {
-    console.log("========= mysqlConnection.js <44> =========");
-    console.log("==> module");
-    console.log(module.exports);
-    console.log("====================================");
-  });
-  return connection;
+    
 }
 
-module.exports = global.get_SQL_Connection();
+module.exports.get_SQL_Connection();
