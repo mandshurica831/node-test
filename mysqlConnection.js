bce3f4923c7c8e88aa5482a67cfc4a262d5880b4
渡辺りえ <watanabe.r@5sg.jp>
2016-09-12 11:54:59 +0900

    もっかい再接続チャレンジ！

--- mysqlConnection.js
+++ mysqlConnection.js
@@ -8,8 +8,38 @@ var dbConfig = {
   database : 'chat_test_160801'
 };
 */
+var db_config = {
+    host     : 'us-cdbr-iron-east-04.cleardb.net',
+    user     : 'b261359fb916b2',
+    password : '61ef1f35',
+    database : 'heroku_6722ee1e07d3f4d'
+};
+var connection; //クライアントオブジェクト
 
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
 
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);  //接続失敗時リトライ
+    }
+  });
+
+  connection.on('error', function(err) { //エラー受け取るコールバック
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.log('== CONECT ==');
+      handleDisconnect();    //再度接続
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+/*
 module.exports.getConnection = function() {
     // Test connection health before returning it to caller.
     if ((module.exports.connection) && (module.exports.connection._socket)
@@ -37,7 +67,13 @@ module.exports.getConnection = function() {
     });
     connection.on("error", function (err) {
         console.log("SQL CONNECTION ERROR: " + err);
-        module.exports.getConnection();
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host     : 'us-cdbr-iron-east-04.cleardb.net',
+            user     : 'b261359fb916b2',
+            password : '61ef1f35',
+            database : 'heroku_6722ee1e07d3f4d'
+        });
     });
     module.exports = connection;
 
@@ -51,3 +87,4 @@ module.exports.getConnection();
 // var database = require(__dirname + "/database");
 // var connection = database.getConnection();
 // connection.query(query, function(err, results) { ....
+*/
